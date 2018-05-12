385232bb9568c0b5a386c2543197b4c405c90a63
Carrie Savinell <carrielane89@gmail.com>
2018-05-12 15:06:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,6 @@ end
 def find_the_cheese(string_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   string_array.detect |cheese|
-    cheese 
+    cheese_types.include?(cheese)
+  end   
 end 
